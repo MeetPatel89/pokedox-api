1922ee79790f43575c81f274bb433ffdda7088d4
Meet Patel <meetaiims@yahoo.com>
2020-11-04 20:33:49 -0500

    After adding .env

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 
+console.log(process.env.API_TOKEN);
 const app = express();
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-    console.log('Middleware callback function triggered!');
-    res.send('Hello, world!');
-});
+function handleGetTypes(req, res) {
+    res.json(validTypes);
+};
+
+app.get('/types', handleGetTypes);
+
 
 const PORT = 3000;
 
