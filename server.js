19e086c01b9c40466eeb082f3b372ec939103643
Meet Patel <meetaiims@yahoo.com>
2020-11-04 19:39:18 -0500

    Check npm commands

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use((req, res) => {
+    console.log('Middleware callback function triggered!');
+    res.send('Hello, world!');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
+
